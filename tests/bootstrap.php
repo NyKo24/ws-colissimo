daf44f0f242d434a59a5594b2ca1d0a814244197
Nicolas Bordes <nicolasbordes@me.com>
2016-11-09 15:46:20 +0100

    Correction test/bootstrap.php"

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
-$loader = require_once __DIR__.'/../vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('WSColissimo\\WSColiPosteLetterService\\Test', __DIR__);
